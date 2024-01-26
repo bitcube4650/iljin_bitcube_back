d9edc161f73f7aaf1e32f49538064399445db631
JimmyKim <kkbum2000@gmail.com>
2024-01-26 12:55:58 +0900

    fix: 기존 소스 오류 수정

--- src/main/java/iljin/framework/core/security/role/RoleRepository.java
+++ src/main/java/iljin/framework/core/security/role/RoleRepository.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, RoleKey> {
-    
-    @Query(value = "select r.* from a_user u inner join a_user_role ur on ur.user_id = u.id inner join a_role r on r.role_cd = ur.`role` where u.login_id = :loginId", nativeQuery = true)
+
+//    @Query(value = "select r.* from a_user u inner join a_user_role ur on ur.user_id = u.id inner join a_role r on r.role_cd = ur.role where u.login_id = :loginId", nativeQuery = true)
+    @Query(value = "select r.* from a_role r", nativeQuery = true)
     List<Role> findByLoginId(String loginId);
 
 }
---
+++ src/main/java/iljin/framework/test/TestController.java
@@ -0,0 +1,32 @@
+package iljin.framework.test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class TestController {
+
+    /**
+     * Front에서 오류 메시지 없애기 위해 
+     */
+    @GetMapping("/dashboard/{compCd}/{loginId}")
+    public ResponseEntity<String> getComboBox(@PathVariable String compCd, @PathVariable String loginId) {
+		return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+    /**
+     * Front에서 오류 메시지 없애기 위해 
+     */
+    @GetMapping("/acctPeriod/openDate")
+    public ResponseEntity<String> openDate() {
+		return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+}
