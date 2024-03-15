516240dcabd789d2c7f9914de927d623b51f1f9a
DESKTOP-T0RA43H\chojunbeom <beom0531@bitcube.co.kr>
2024-03-15 16:33:30 +0900

    입찰계획>입찰상세 엑셀다운로드

--- src/main/java/iljin/framework/ebid/etc/util/common/excel/controller/ExcelController.java
+++ src/main/java/iljin/framework/ebid/etc/util/common/excel/controller/ExcelController.java
@@ -1,14 +1,17 @@
 package iljin.framework.ebid.etc.util.common.excel.controller;
 
+import iljin.framework.ebid.etc.util.common.excel.dto.BidProgressResponseDto;
 import iljin.framework.ebid.etc.util.common.excel.service.ExcelService;
 import iljin.framework.ebid.etc.util.common.excel.repository.ExcelRepository;
 import iljin.framework.ebid.etc.util.common.excel.utils.ExcelUtils;
 import iljin.framework.ebid.etc.util.common.excel.entity.FileEntity;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +19,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/excel")
+@Slf4j
 public class ExcelController {
     private final ExcelService excelService;
     private final ExcelRepository excelRepository;
@@ -28,14 +33,24 @@ public class ExcelController {
      *  MVC MODEL에 의거한 공통화 모듈.
      *
      */
-    @PostMapping("/downLoad")
-    public ResponseEntity excelDownLoadMVC(HttpServletResponse response) {
+   @PostMapping("/downLoadExcelCommon")
+    public ResponseEntity excelDownLoadMVC2(HttpServletResponse response) {
         try {
-            excelService.downLoadCommon(response);
+            excelService.downloadCommonExcel(response);
         } catch(Exception e) {
             String msg = " ExcelDownLoad를 실패하였습니다.";
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage() + msg);
         }
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+
+    //전자입찰>입찰계획>입찰계획상세 Excel DownLoad
+    @PostMapping("/bid/progressDetail/downLoad")
+    public ResponseEntity downLoadExcelBidProgressDetail(HttpServletResponse response,
+                                                         @RequestBody BidProgressResponseDto responseDto) throws IOException {
+       excelService.downloadBidProgressExcel(responseDto, response);
+
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
--- src/main/java/iljin/framework/ebid/etc/util/common/excel/utils/ExcelUtils.java
+++ src/main/java/iljin/framework/ebid/etc/util/common/excel/utils/ExcelUtils.java
@@ -1,20 +1,26 @@
 package iljin.framework.ebid.etc.util.common.excel.utils;
 
 import com.opencsv.CSVWriter;
+import iljin.framework.ebid.etc.util.CommonUtils;
+import iljin.framework.ebid.etc.util.common.excel.dto.BidProgressResponseDto;
 import iljin.framework.ebid.etc.util.common.excel.repository.ExcelRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.lang.reflect.Field;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -280,6 +286,146 @@ public final class ExcelUtils implements ExcelSupport {
             listStrings.add(rowData);
         }
         return listStrings;
+    }
+
+    //전자입찰>입찰계획>입찰계획상세 Excel DownLoad
+    public void createBidProgressDetailExcel(BidProgressResponseDto param, HttpServletResponse response) throws IOException {
+        InputStream fis = getClass().getResourceAsStream("/menual/bidProgressDetail.xlsx");
+        XSSFWorkbook xssfWorkBook = new XSSFWorkbook(fis);
+        XSSFSheet xssfSheet = xssfWorkBook.getSheetAt(0);
+
+        String fileName = param.getFileName();
+        fileName = URLEncoder.encode(fileName,"UTF-8").replaceAll("\\+", "%20");
+
+        Map<String, Object> result = param.getResult();                     //엑셀 모든 컬럼
+        List<Map<String, Object>> custContent = param.getCustContent();     //입찰참가업체 컬럼
+        List<Map<String, Object>> tableContent = param.getTableContent();   //직접등록시 내역사항에 들어가는 세부사항 컬럼
+        List<Map<String, Object>> fileContent = param.getFileContent();     //파일등록시 내역사항에 들어가는 파일리스트 컬럼
+
+        /*
+         *  입찰참가 업체 컬럼 생성
+         */
+        StringBuilder custNameBuilder = new StringBuilder();
+        int index = 0;
+
+        for(Map<String, Object> map : custContent) {
+            custNameBuilder.append(map.get("custName"));
+            // 다음 요소가 있으면 쉼표 추가
+            if (++index < custContent.size()) {
+                custNameBuilder.append(", ");
+            }
+        }
+
+        /*
+         *  Flag : 직접등록, 파일등록
+         *  직접등록시 내역사항에 세부사항   들어감
+         *  파일등록시 내역사항에 파일리스트 들어감
+         */
+        String insModeFlag = String.valueOf(result.get("insMode"));
+        StringBuilder insModeContentBuilder = new StringBuilder();
+
+        //단가에 통화 포멧팅
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+
+        if("직접입력".equals(insModeFlag)) {
+            //직접등록시 내역사항에 들어가는 세부사항
+            for(Map<String, Object> map : tableContent) {
+                insModeContentBuilder.append(map.get("name"));
+                insModeContentBuilder.append("     ");
+                insModeContentBuilder.append(map.get("ssize"));
+                insModeContentBuilder.append("     ");
+                insModeContentBuilder.append(map.get("orderQty"));
+                insModeContentBuilder.append(map.get("unitcode"));
+                insModeContentBuilder.append("     ");
+
+                //단가에 통화 포멧팅
+                String orderUc = decimalFormat.format(map.get("orderUc"));
+                insModeContentBuilder.append("\\");
+                insModeContentBuilder.append(orderUc);
+                insModeContentBuilder.append("\n");
+            }
+        } else {
+            //파일등록시 내역사항에 들어가는 파일리스트
+            index = 0;
+            for(Map<String, Object> map : fileContent) {
+                insModeContentBuilder.append(map.get("fileNm"));
+                // 다음 요소가 있으면 공백 추가
+                if (++index < fileContent.size()) {
+                    insModeContentBuilder.append("\n");
+                }
+            }
+        }
+
+        /*
+         ** 입찰 기본정보
+         */
+        String biNo         = CommonUtils.getString(result.get("biNo"), "");             //입찰번호
+        String biName       = CommonUtils.getString(result.get("biName"), "");           //입찰명
+        String itemName     = CommonUtils.getString(result.get("itemName"), "");         //품목
+        String biMode       = CommonUtils.getString(result.get("biMode"), "");           //입찰방식
+        String bidJoinSpec  = CommonUtils.getString(result.get("bidJoinSpec"), "");      //입찰참가자격
+        String specialCond  = CommonUtils.getString(result.get("specialCond"), "");      //특수조건
+        String spotDate     = CommonUtils.getString(result.get("spotDate"), "");         //현장설명일시
+        String spotArea     = CommonUtils.getString(result.get("spotArea"), "");         //현장설명장소
+        String succDeciMeth = CommonUtils.getString(result.get("succDeciMeth"), "");     //낙찰자결정방법
+        String custName     = CommonUtils.getString(custNameBuilder, "");                //입찰참가업체
+        String amtBasis     = CommonUtils.getString(result.get("amtBasis"), "");         //금액기준
+        String payCond      = CommonUtils.getString(result.get("payCond"), "");          //결제조건
+        String bdAmt        = CommonUtils.getString(result.get("bdAmt"), "");            //예산금액
+        String cuser        = CommonUtils.getString(result.get("cuser"), "");            //입찰담당자
+
+        /*
+         ** 입찰 공고 추가등록 사항
+         */
+        String estStartDate    = CommonUtils.getString(result.get("estStartDate"), "");  //제출 시작일시
+        String estCloseDate    = CommonUtils.getString(result.get("estCloseDate"), "");  //제출 마감일시
+        String estOpener       = CommonUtils.getString(result.get("estOpener"), "");     //개찰자, 낙찰자
+        String gongoId         = CommonUtils.getString(result.get("gongoId"), "");       //입찰 공고자
+        String openAtt1        = CommonUtils.getString(result.get("openAtt1"), "");      //입회자1
+        String openAtt2        = CommonUtils.getString(result.get("openAtt2"), "");      //입회자2
+        String supplyCond      = CommonUtils.getString(result.get("supplyCond"), "");    //납품 조건
+        String insMode         = CommonUtils.getString(result.get("insMode"), "");       //내역 방식
+        String insModeContent  = CommonUtils.getString(insModeContentBuilder, "");       //내역사항
+
+        /*
+        ** 입찰 기본정보
+         */
+        xssfSheet.getRow(3).getCell(1).setCellValue(biNo);          //입찰번호
+        xssfSheet.getRow(4).getCell(1).setCellValue(biName);        //입찰명
+        xssfSheet.getRow(5).getCell(1).setCellValue(itemName);      //품목
+        xssfSheet.getRow(6).getCell(1).setCellValue(biMode);        //입찰방식
+        xssfSheet.getRow(7).getCell(1).setCellValue(bidJoinSpec);   //입찰참가자격
+        xssfSheet.getRow(8).getCell(1).setCellValue(specialCond);   //특수조건
+        xssfSheet.getRow(9).getCell(1).setCellValue(spotDate);      //현장설명일시
+        xssfSheet.getRow(10).getCell(1).setCellValue(spotArea);     //현장설명장소
+        xssfSheet.getRow(11).getCell(1).setCellValue(succDeciMeth); //낙찰자결정방법
+        xssfSheet.getRow(12).getCell(1).setCellValue(custName);     //입찰참가업체
+        xssfSheet.getRow(13).getCell(1).setCellValue(amtBasis);     //금액기준
+        xssfSheet.getRow(14).getCell(1).setCellValue(payCond);      //결제조건
+        xssfSheet.getRow(15).getCell(1).setCellValue(bdAmt);        //예산금액
+        xssfSheet.getRow(16).getCell(1).setCellValue(cuser);        //입찰담당자
+       /*
+       ** 입찰 공고 추가등록 사항
+        */
+        xssfSheet.getRow(19).getCell(1).setCellValue(estStartDate);  //제출시작일시
+        xssfSheet.getRow(20).getCell(1).setCellValue(estCloseDate);  //제출마감일시
+        xssfSheet.getRow(21).getCell(1).setCellValue(estOpener);     //개찰자/낙찰자
+        xssfSheet.getRow(22).getCell(1).setCellValue(gongoId);       //입찰공고자
+        xssfSheet.getRow(23).getCell(1).setCellValue(openAtt1);      //입회자1
+        xssfSheet.getRow(24).getCell(1).setCellValue(openAtt2);      //입회자2
+        xssfSheet.getRow(25).getCell(1).setCellValue(supplyCond);    //납품조건
+        xssfSheet.getRow(26).getCell(1).setCellValue(insMode);       //내역방식
+        xssfSheet.getRow(27).getCell(1).setCellValue(insModeContent);//내역사항
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setHeader("Content-Disposition", "attachment; filename=" + fileName + ".xlsx");
+
+        ServletOutputStream outputStream = response.getOutputStream();
+        xssfWorkBook.write(outputStream);
+        xssfWorkBook.close();
+        outputStream.flush();
+        outputStream.close();
     }
 }
 
