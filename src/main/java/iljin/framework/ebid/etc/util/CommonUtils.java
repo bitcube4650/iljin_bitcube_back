005205513fd663faf9a0130a34ed06316f31ed9a
DESKTOP-T0RA43H\chojunbeom <beom0531@bitcube.co.kr>
2024-03-20 16:41:30 +0900

    메일 형식 수정.

--- src/main/java/iljin/framework/ebid/etc/util/CommonUtils.java
+++ src/main/java/iljin/framework/ebid/etc/util/CommonUtils.java
@@ -95,52 +95,25 @@ public class CommonUtils {
         StringBuffer mailContents = new StringBuffer();
         mailContents.append("<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>");
         mailContents.append("<html xmlns='http://www.w3.org/1999/xhtml'>");
-        mailContents.append("<head>");
-        mailContents.append("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
-        mailContents.append("<title>OKPlaza</title>");
-        mailContents.append("</head>");
-        mailContents.append("<body style='margin:0;padding:0'>");
-
-        mailContents.append("	<table cellspacing='0' style='width:700px;padding:0px;background:#fff;border:2px solid #F53243;'>");
-
-        //HEADER
-        mailContents.append("		<tr>");
-        mailContents.append("			<td style='padding:0 0 0 25px;background:#F53243;border:2px solid #F53243;height:50px;overflow:hidden;text-align:center;' nowrap>");
-        mailContents.append("			</td>");
-        mailContents.append("			<td style='padding:0 25px 0 0;background:#F53243;border:2px solid #F53243;width:300px;height:50px;overflow:hidden;font-size:20px;font-family:\"Malgun Gothic\";font-weight:bold;' nowrap>");
-        mailContents.append("				<span style='margin-left:10px; color:#fff;'>E-mail 서비스 입니다.</span>");
-        mailContents.append("			</td>");
-        mailContents.append("			<td style='background:#F53243;height:50px;overflow:hidden;border:2px solid #F53243;width:310px;' nowrap></td>");
-        mailContents.append("		</tr>");
-
-        //CONTENT
-        mailContents.append("		<tr>");
-        mailContents.append("			<td colspan='3' style='margin:0;padding:20px 55px 10px;background:#fff;font-size:14px;line-height:20px;font-family:\"Malgun Gothic\";letter-spacing:-1px;font-weight:bold;color:#343434;word-break:break-all'>");
-        mailContents.append("안녕하십니까<br>" +
-                            "일진그룹 전자입찰 e-bidding입니다.<br>");
-        mailContents.append("<br>");
+        mailContents.append("<head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'></head>");
+        mailContents.append("<body style='background:#f2f3f6'>");
+        mailContents.append("    <link href='./mail01_files/css2' rel='stylesheet'>");
+        mailContents.append("    <div style='width:600px; min-height:600px; margin:20px auto; background:#fff; padding:40px 40px 100px 40px; box-sizing:border-box; position:relative; font-family:\"Noto Sans KR\", sans-serif'>");
+        mailContents.append("        <div style='border-bottom:1px solid #9F9F9F; padding-bottom:30px; margin-bottom:30px'><img src='./mail01_files/loginLogo_iljin.svg' alt='일진' style='height:40px'></div>");
+        mailContents.append("        <div style='font-size:18px; font-weight:700; line-height:150%'>");
+        mailContents.append("            안녕하십니까<br>");
+        mailContents.append("            일진그룹 전자입찰 e-bidding 입니다.<br>");
+        mailContents.append("        </div>");
+        mailContents.append("        <div style='font-size:16px; line-height:150%; margin-top:30px'>");
         mailContents.append(contents + "<br>");
-        mailContents.append("<br>");
-        mailContents.append("감사합니다.");
-        mailContents.append("<br>");
-        mailContents.append("			</td>");
-        mailContents.append("		</tr>");
-
-        //FOOTER
-        mailContents.append("		<tr>");
-        mailContents.append("			<td colspan='3' style='padding:0 25px;overflow:hidden;background:#fff;' nowrap>");
-        mailContents.append("				<hr style='width:650px;border:1px solid #F53243;overflow:hidden;color:#F53243;'>");
-        mailContents.append("			</td>");
-        mailContents.append("		</tr>");
-        mailContents.append("		<tr>");
-        mailContents.append("			<td colspan='3' style='padding:0px 25px 5px;overflow:hidden;background:#fff;text-align:right;' nowrap>");
-        mailContents.append("			</td>");
-        mailContents.append("		</tr>");
-
-        mailContents.append("	</table>");
-
-        mailContents.append("</body>");
-        mailContents.append("</html>");
+        mailContents.append("            자세한 사항은 <a href='https://iljin.idr.myds.me/login.html' target='_blank' style='color:#004B9E !important; font-weight:700; text-decoration:none; border-bottom:1px solid #004B9E'>e-bidding</a> 시스템에 로그인하여 확인해 주십시오<br><br><br>");
+        mailContents.append("            감사합니다.");
+        mailContents.append("        </div>");
+        mailContents.append("        <div style='position:absolute; left:0; bottom:0; width:100%; box-sizing:border-box; padding:0 40px'>");
+        mailContents.append("            <div style='border-top:1px solid #E6E6E6; color:#858585; font-size:12px; padding:20px 0 25px 0'>© ILJIN ALL RIGHTS RESERVED.</div>");
+        mailContents.append("        </div>");
+        mailContents.append("    </div>");
+        mailContents.append("</body></html>");
 
         return mailContents.toString();
     }
