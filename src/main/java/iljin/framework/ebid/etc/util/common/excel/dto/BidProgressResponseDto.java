e2586ba3a9035a0a9fee59ab8a703e096b823299
ehd123309 <ehdgus309@bitcube.co.kr>
2024-03-27 23:31:10 +0900

    입찰계획 상세 엑셀변환 오류 해결

--- src/main/java/iljin/framework/ebid/etc/util/common/excel/dto/BidProgressResponseDto.java
+++ src/main/java/iljin/framework/ebid/etc/util/common/excel/dto/BidProgressResponseDto.java
@@ -2,12 +2,14 @@ package iljin.framework.ebid.etc.util.common.excel.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BidProgressResponseDto {
 
     private String fileName;                            //파일이름
@@ -15,4 +17,5 @@ public class BidProgressResponseDto {
     private List<Map<String, Object>> tableContent;     //직접등록시 내역사항에 들어가는 세부사항 컬럼
     private List<Map<String, Object>> fileContent;      //파일등록시 내역사항에 들어가는 파일리스트 컬럼
     private List<Map<String, Object>> custContent;      //입찰참가업체 컬럼
+    
 }
