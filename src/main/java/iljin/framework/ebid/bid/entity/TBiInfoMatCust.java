cf6d03a94412634ce578e07744425cc8ac558677
ehd123309 <ehdgus309@bitcube.co.kr>
2024-03-29 23:19:54 +0900

    투찰에 필요한 entity 및 레파지토리

---
+++ src/main/java/iljin/framework/ebid/bid/entity/TBiInfoMatCust.java
@@ -0,0 +1,73 @@
+package iljin.framework.ebid.bid.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import iljin.framework.ebid.etc.notice.entity.TCoBoardCustID;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@IdClass(TBiInfoMatCustID.class)
+@Table(name = "t_bi_info_mat_cust")
+public class TBiInfoMatCust {
+
+	@Id
+    @Column(name = "bi_no")
+    String biNo;
+	
+	@Id
+    @Column(name = "cust_code")
+    Integer custCode;
+	
+	@Column(name="esmt_yn")
+	String esmtYn;
+	
+	@Column(name="succ_yn")
+	String succYn;
+	
+	@Column(name = "file_id")
+    Integer fileId;
+	
+	@Column(name="submit_date")
+	LocalDateTime submitDate;
+	
+	@Column(name="create_user")
+	String createUser;
+	
+	@Column(name="create_date")
+	LocalDateTime createDate;
+	
+	@Column(name="update_user")
+	String updateUser;
+	
+	@Column(name="update_date")
+	LocalDateTime updateDate;
+	
+	@Column(name = "bi_order")
+    Integer biOrder;
+	
+	@Column(name="enc_qutn")
+	String encQutn;
+	
+	@Column(name="enc_esmt_spec")
+	String encEsmtSpec;
+	
+	@Column(name="etc_b_file")
+	String etcBFile;
+	
+	@Column(name="file_hash_value")
+	String fileHashValue;
+	
+	@Column(name="etc_b_file_path")
+	String etcBFilePath;
+	
+	
+}
---
+++ src/main/java/iljin/framework/ebid/bid/entity/TBiLog.java
@@ -0,0 +1,36 @@
+package iljin.framework.ebid.bid.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import iljin.framework.ebid.etc.notice.entity.TCoBoardNotice;
+import lombok.Data;
+
+@Entity
+@Table(name = "t_bi_log")
+@Data
+public class TBiLog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@Column(name="log_seq")
+	Integer logSeq;
+	
+	@Column(name="bi_no")
+	String biNo;
+	
+	@Column(name="user_id")
+	String userId;
+	
+	@Column(name="log_text")
+	String logText;
+	
+	@Column(name="create_date")
+	LocalDateTime createDate;
+}
