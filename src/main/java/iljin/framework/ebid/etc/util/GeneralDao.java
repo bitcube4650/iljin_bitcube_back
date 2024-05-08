d772ab7432b7e05fbcda6077e01aabbc1da254f3
JAR <areum@bitcube.co.kr>
2024-05-08 10:55:48 +0900

    입찰계획 리스트 조회 mybatis 변환

--- src/main/java/iljin/framework/ebid/etc/util/GeneralDao.java
+++ src/main/java/iljin/framework/ebid/etc/util/GeneralDao.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import lombok.extern.log4j.Log4j2;
@@ -47,5 +50,18 @@ public class GeneralDao {
 		}
 		return result;
 	}
-    
+	
+	@SuppressWarnings("unchecked")
+	public Page selectGernalListPage(String queryId, @SuppressWarnings("rawtypes") Map paramMap) throws Exception {
+		int				count		= 0;
+		List<Object>	list		= null;
+		Pageable		pageable	= PagaUtils.pageable(paramMap);
+		paramMap.put("offset", pageable.getOffset());
+		paramMap.put("pageSize", pageable.getPageSize());
+		
+		count = sqlSession.selectOne(queryId + "_count", paramMap);
+		list = sqlSession.selectList(queryId, paramMap);
+		
+		return new PageImpl<>(list, pageable, count);
+	}
 }
