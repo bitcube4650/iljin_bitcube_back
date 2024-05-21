107d60831db1f3ddec58f35714f3466822974bfe
이의진 <destiny09090@bitcube.co.kr>
2024-05-21 16:28:51 +0900

    정보관리 > 사용자관리 커밋

--- src/main/java/iljin/framework/ebid/custom/controller/CoUserController.java
+++ src/main/java/iljin/framework/ebid/custom/controller/CoUserController.java
@@ -1,11 +1,9 @@
 package iljin.framework.ebid.custom.controller;
 
 import iljin.framework.core.dto.ResultBody;
-import iljin.framework.ebid.custom.dto.TCoUserDto;
 import iljin.framework.ebid.custom.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -33,8 +31,15 @@ public class CoUserController {
 		return resultBody;
 	}
 
-	// 사용자 리스트
-    @PostMapping("/userList")
+	/**
+	 * 사용자 리스트
+	 * @param params interrelatedCustCode
+	 * @param params useYn
+	 * @param params userName
+	 * @param params userId
+	 * @return
+	 */
+	@PostMapping("/userList")
 	public ResultBody userList(@RequestBody Map<String, Object> params) {
 		ResultBody resultBody = new ResultBody();
 		try {
@@ -46,14 +51,21 @@ public class CoUserController {
 		}
 		return resultBody;
 	}
-
-	// 사용자 상세 비밀번호 확인
+	/**
+	 * 사용자 상세 비밀번호 확인
+	 * @param params pwd
+	 * @return
+	 */
 	@PostMapping("/pwdCheck")
 	public ResultBody pwdCheck(@RequestBody Map<String, Object> params) {
 		return userService.pwdCheck(params);
 	}
 
-	// 사용자 상세
+	/**
+	 * 사용자 상세
+	 * @param params userId
+	 * @return
+	 */
     @PostMapping("/userDetail")
 	public ResultBody userDetail(@RequestBody Map<String, Object> params) {
 		ResultBody resultBody = new ResultBody();
@@ -67,14 +79,54 @@ public class CoUserController {
 		return resultBody;
 	}
 
-    @PostMapping("/save")
-    public ResultBody save(@RequestBody Map<String, Object> params) {
-        return userService.save(params);
+	/**
+	 * 사용자 저장/수정
+	 * @param params userId
+	 * @param params userPwd
+	 * @param params userName
+	 * @param params interrelatedCustCode
+	 * @param params userAuth
+	 * @param params openauth
+	 * @param params bidauth
+	 * @param params userHp
+	 * @param params userTel
+	 * @param params userEmail
+	 * @param params userPosition
+	 * @param params deptName
+	 * @param params useYn
+	 * @param params userInterrelatedList
+	 * @return
+	 */
+    @PostMapping("/userSave")
+    public ResultBody userSave(@RequestBody Map<String, Object> params) {
+		ResultBody resultBody = new ResultBody();
+		try {
+			resultBody = userService.userSave(params);
+		}catch(Exception e) {
+			log.error("userSave error : {}", e);
+			resultBody.setCode("fail");
+			resultBody.setMsg("사용자 저장에 실패하였습니다.");
+		}
+		return resultBody;
     }
 
-    @PostMapping("/saveChgPwd")
-    public ResultBody saveChgPwd(@RequestBody Map<String, Object> params) {
-        return userService.saveChgPwd(params);
+	/**
+	 * 비밀번호 변경
+	 * @param params userId
+	 * @param params chgPassword
+	 * @return
+	 */
+	@PostMapping("/saveChgPwd")
+	public ResultBody saveChgPwd(@RequestBody Map<String, Object> params) {
+		ResultBody resultBody = new ResultBody();
+		try {
+			resultBody = userService.saveChgPwd(params);
+		}catch(Exception e) {
+			log.error("saveChgPwd error : {}", e);
+			resultBody.setCode("fail");
+			resultBody.setMsg("비밀번호 변경에 실패하였습니다.");
+		}
+		return resultBody;
     }
 
 
--- src/main/java/iljin/framework/ebid/custom/service/UserService.java
+++ src/main/java/iljin/framework/ebid/custom/service/UserService.java
@@ -1,37 +1,25 @@
 package iljin.framework.ebid.custom.service;
 
-import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import iljin.framework.core.dto.ResultBody;
+import iljin.framework.ebid.custom.entity.TCoUser;
+import iljin.framework.ebid.custom.repository.TCoUserRepository;
+import iljin.framework.ebid.etc.util.CommonUtils;
 import iljin.framework.ebid.etc.util.GeneralDao;
-import org.qlrm.mapper.JpaResultMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
-import iljin.framework.core.dto.ResultBody;
-import iljin.framework.core.util.Pair;
-import iljin.framework.ebid.custom.dto.TCoUserDto;
-import iljin.framework.ebid.custom.entity.TCoUser;
-import iljin.framework.ebid.custom.repository.TCoUserRepository;
-import iljin.framework.ebid.etc.util.CommonUtils;
-import iljin.framework.ebid.etc.util.PagaUtils;
-import lombok.extern.slf4j.Slf4j;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -88,78 +76,43 @@ public class UserService {
 		return resultBody;
 	}
 
-    @Transactional
-    public ResultBody save(Map<String, Object> params) {
-        ResultBody resultBody = new ResultBody();
-        StringBuilder sbQuery = null;
-     // 저장
-        if ((boolean) params.get("isCreate")) {
-            sbQuery = new StringBuilder(
-            " insert into t_co_user (user_id, user_pwd, user_name, interrelated_cust_code, user_auth, openauth, bidauth, user_hp, user_tel, user_email, user_position, dept_name, use_yn, create_user, create_date, update_user, update_date, pwd_edit_date, pwd_edit_yn) " +
-            " values (:userId, :userPwd, :userName, :interrelatedCustCode, :userAuth, :openauth, :bidauth, :userHp, :userTel, :userEmail, :userPosition, :deptName, :useYn, :updateUser, now(), :updateUser, now(), now(), 'N')");
-        } 
-        // 수정
-        else {
-            sbQuery = new StringBuilder(
-            " update t_co_user "
-            + "set user_name = :userName"
-            + ", interrelated_cust_code = :interrelatedCustCode"
-            + ", user_auth = :userAuth"
-            + ", openauth = :openauth"
-            + ", bidauth = :bidauth"
-            + ", user_hp = :userHp"
-            + ", user_tel = :userTel"
-            + ", user_email = :userEmail"
-            + ", user_position = :userPosition"
-            + ", dept_name = :deptName"
-            + ", use_yn = :useYn"
-            + ", update_user = :updateUser"
-            + ", update_date = now() "
-            + "where user_id = :userId");
-        }
-        Query query = entityManager.createNativeQuery(sbQuery.toString());
-        query.setParameter("userId", params.get("userId"));
-        if ((boolean) params.get("isCreate")) {
-        	// 비밀번호 암호화
-        	String userPwd = CommonUtils.getString(params.get("userPwd"), "");
-        	String encodedPassword = passwordEncoder.encode(userPwd);
-            query.setParameter("userPwd", encodedPassword);
-        }
-        query.setParameter("userName", params.get("userName"));
-        query.setParameter("interrelatedCustCode", params.get("interrelatedCustCode"));
-        query.setParameter("userAuth", params.get("userAuth"));
-        query.setParameter("openauth", params.get("openauth"));
-        query.setParameter("bidauth", params.get("bidauth"));
-        query.setParameter("userHp", CommonUtils.getString(params.get("userHp")).replace("-",""));
-        query.setParameter("userTel", CommonUtils.getString(params.get("userTel")).replace("-",""));
-        query.setParameter("userEmail", params.get("userEmail"));
-        query.setParameter("userPosition", params.get("userPosition"));
-        query.setParameter("deptName", params.get("deptName"));
-        query.setParameter("useYn", params.get("useYn"));
-        String createUser = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        query.setParameter("updateUser", createUser);
-        query.executeUpdate();
+	@Transactional
+	public ResultBody userSave(Map<String, Object> params) throws Exception {
+		ResultBody resultBody = new ResultBody();
+		// 등록/수정자
+		String createUser = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+		params.put("createUser", createUser);
+		params.put("updateUser", createUser);
+		// 저장
+		if ((boolean) params.get("isCreate")) {
+			// 비밀번호 암호화
+			String userPwd = CommonUtils.getString(params.get("userPwd"), "");
+			String encodedPassword = passwordEncoder.encode(userPwd);
+			params.put("encodedPassword", encodedPassword);
 
-        // 고유 키가 없기에 매번 지워야 한다.
-        sbQuery = new StringBuilder(" delete from t_co_user_interrelated where user_id = :userId");
-        query = entityManager.createNativeQuery(sbQuery.toString());
-        query.setParameter("userId", params.get("userId"));
-        query.executeUpdate();
-        // 감사사용자의 경우 감사계열사 정보를 저장 처리
-        if ("4".equals(params.get("userAuth"))) {
-            List<Map> list = (List) params.get("userInterrelatedList");
-            for (Map<String, Object> data : list) {
-                if (data.get("check") != null && (boolean)data.get("check") == true) {
-                    sbQuery = new StringBuilder(" insert into t_co_user_interrelated (interrelated_cust_code, user_id) values (:interrelatedCustCode, :userId)");
-                    query = entityManager.createNativeQuery(sbQuery.toString());
-                    query.setParameter("interrelatedCustCode", data.get("key"));
-                    query.setParameter("userId", params.get("userId"));
-                    query.executeUpdate();
-                }
-            }
-        }
-        return resultBody;
-    }
+			generalDao.insertGernal("insertUserSave", params);
+		}
+		// 수정
+		else {
+			generalDao.updateGernal("updateUserSave", params);
+		}
+		// 계열사 등록
+		// 고유 키가 없기에 매번 지워야 한다.
+		generalDao.deleteGernal("deleteUserInterrelated", params);
+		// 감사사용자의 경우 감사계열사 정보를 저장 처리
+		if ("4".equals(params.get("userAuth"))) {
+			List<Map> list = (List) params.get("userInterrelatedList");
+			for (Map<String, Object> dataMap : list) {
+				//선택한 계열사만 인서트
+				if (dataMap.get("check") != null && (boolean)dataMap.get("check") == true) {
+					dataMap.put("interrelatedCustCode", dataMap.get("interrelatedCustCode"));
+					dataMap.put("userId", params.get("userId"));
+					generalDao.insertGernal("insertUserInterrelated", dataMap);
+				}
+			}
+		}
+		return resultBody;
+	}
     // 비밀번호 체크
 	public ResultBody pwdCheck(Map<String, Object> params) {
         ResultBody resultBody = new ResultBody();
@@ -187,26 +140,19 @@ public class UserService {
     
     // 비밀번호 변경
     @Transactional
-	public ResultBody saveChgPwd(Map<String, Object> params) {
-        ResultBody resultBody = new ResultBody();
-        StringBuilder sbQuery = new StringBuilder(
-	            " update t_co_user "
-	            + "set user_pwd = :userPwd"
-	            + ", pwd_edit_yn = 'Y'"
-	            + ", pwd_edit_date = now()"
-	            + ", update_user = :updateUser"
-	            + ", update_date = now() "
-	            + "where user_id = :userId");
-        Query query = entityManager.createNativeQuery(sbQuery.toString());
-        query.setParameter("userId", params.get("userId"));
-        // 비밀번호 암호화
-        String chgPassword = CommonUtils.getString(params.get("chgPassword"), "");
-        String encodedPassword = passwordEncoder.encode(chgPassword);
-        query.setParameter("userPwd", encodedPassword);
-        String createUser = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
-        query.setParameter("updateUser", createUser);
-        query.executeUpdate();
-        
+	public ResultBody saveChgPwd(Map<String, Object> params) throws Exception {
+		ResultBody resultBody = new ResultBody();
+
+		// 비밀번호 암호화
+		String chgPassword = CommonUtils.getString(params.get("chgPassword"), "");
+		String encodedPassword = passwordEncoder.encode(chgPassword);
+		params.put("userPwd", encodedPassword);
+
+		String createUser = ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+		params.put("userPwd", encodedPassword);
+
+		generalDao.updateGernal("user.updateUserChgPwd",params);
+
 		return resultBody;
 	}
 }
