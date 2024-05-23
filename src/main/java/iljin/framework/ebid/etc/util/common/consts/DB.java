1dfa3c26eba7e0eca08ca7f85edc89e7538e22f4
강민지 <lami34@bitcube.co.kr>
2024-05-23 16:29:30 +0900

    입찰진행, 입찰완료 DB 상수화

--- src/main/java/iljin/framework/ebid/etc/util/common/consts/DB.java
+++ src/main/java/iljin/framework/ebid/etc/util/common/consts/DB.java
@@ -75,4 +75,58 @@ public class DB {
 	public final static String QRY_INSERT_FAQ = "etc.insertFaq";
 	public final static String QRY_UPDATE_FAQ = "etc.updateFaq";
 	public final static String QRY_DELETE_FAQ = "etc.deleteFaq";
+	
+	//bid_status
+	public final static String QRY_SELECT_EBID_STATUS_LIST = "bidStatus.selectEbidStatusList";
+	public final static String QRY_SELECT_EBID_STATUS_DETAIL = "bidStatus.selectEbidStatusDetail";
+	public final static String QRY_SELECT_EBID_STATUS_JOIN_CUST_LIST = "bidStatus.selectEbidStatusJoinCustList";
+	public final static String QRY_SELECT_EBID_STATUS_JOIN_CUST_SPEC = "bidStatus.selectEbidStatusJoinCustSpec";
+	public final static String QRY_SELECT_EBID_STATUS_DETAIL_FILE = "bidStatus.selectEbidStatusDetailFile";
+	public final static String QRY_SELECT_EBID_STATUS_DETAIL_SPEC = "bidStatus.selectEbidStatusDetailSpec";
+	public final static String QRY_UPDATE_EBID_STATUS = "bidStatus.updateEbidStatus";
+	public final static String QRY_SELECT_EBID_BI_MODE_A_SEND_INFO = "bidStatus.selectEbidBiModeASendInfo";
+	public final static String QRY_SELECT_EBID_BI_MODE_B_SEND_INFO = "bidStatus.selectEbidBiModeBSendInfo";
+	public final static String QRY_SELECT_DECRYPT_EBID_CUST_LIST = "bidStatus.selectDecryptEbidCustList";
+	public final static String QRY_INSERT_T_BI_DETAIL_MAT_CUST = "bidStatus.insertTBiDetailMatCust";
+	public final static String QRY_INSERT_T_BI_DETAIL_MAT_CUST_TEMP = "bidStatus.insertTBiDetailMatCustTemp";
+	public final static String QRY_UPDATE_OPEN_EBID_T_BI_INFO_MAT_CUST = "bidStatus.updateOpenEbidTBiInfoMatCust";
+	public final static String QRY_UPDATE_OPEN_EBID_T_BI_INFO_MAT = "bidStatus.updateOpenEbidTBiInfoMat";
+	public final static String QRY_UPDATE_EBID_SUCCESS_T_BI_INFO_MAT = "bidStatus.updateEbidSuccessTBiInfoMat";
+	public final static String QRY_UPDATE_EBID_SUCCESS_T_BI_INFO_MAT_CUST = "bidStatus.updateEbidSuccessTBiInfoMatCust";
+	public final static String QRY_UPDATE_EBID_SUCCESS_T_BI_INFO_MAT_CUST_TEMP = "bidStatus.updateEbidSuccessTBiInfoMatCustTemp";
+	public final static String QRY_SELECT_EBID_BI_MODE_A_SEND_INFO_CUST_LIST = "bidStatus.selectEbidBiModeASendInfoCustList";
+	public final static String QRY_SELECT_EBID_BI_MODE_B_SEND_INFO_CUST_LIST = "bidStatus.selectEbidBiModeBSendInfoCustList";
+	public final static String QRY_INSERT_T_BI_INFO_MAT_HIST = "bidStatus.insertTBiInfoMatHist";
+	public final static String QRY_UPDATE_REBID_T_BI_INFO_MAT = "bidStatus.updateRebidTBiInfoMat";
+	public final static String QRY_UPDATE_REBID_ATT_N = "bidStatus.updateRebidAttN";
+	public final static String QRY_DELETE_T_BI_DETAIL_MAT_CUST_CUST_CODE = "bidStatus.deleteTBiDetailMatCustCustCode";
+	public final static String QRY_UPDATE_REBID_T_BI_INFO_MAT_CUST_CUST_CODE = "bidStatus.updateRebidTBiInfoMatCustCustCode";
+	public final static String QRY_INSERT_T_BI_INFO_MAT_CUST_TEMP = "bidStatus.insertTBiInfoMatCustTemp";
+	public final static String QRY_SELECT_T_BI_INFO_MAT_CUST_TEMP_CUST_CODE = "bidStatus.selectTBiInfoMatCustTempCustCode";
+	public final static String QRY_UPDATE_OPEN_ATT_SIGN = "bidStatus.updateOpenAttSign";
+	public final static String QRY_SELECT_T_BI_INFO_MAT_INFOMATION = "bidStatus.selectTBiInfoMatInfomation";
+	public final static String QRY_BID_STATUS_INSERT_T_BI_LOG = "bidStatus.insertTBiLog";
+	public final static String QRY_SELECT_PARTNER_EBID_STATUS_LIST = "bidStatus.selectPartnerEbidStatusList";
+	public final static String QRY_UPDATE_EBID_T_BI_INFO_MAT_CUST_CONFIRM = "bidStatus.updateEbidTBiInfoMatCustConfirm";
+	public final static String QRY_SELECT_PARTNER_EBID_STATUS_DETAIL = "bidStatus.selectPartnerEbidStatusDetail";
+	public final static String QRY_SELECT_CODE_RATE_LIST = "bidStatus.selectCodeRateList";
+	public final static String QRY_INSERT_T_BI_UPLOAD_C = "bidStatus.insertTBiUploadC";
+	public final static String QRY_MERGED_T_BI_INFO_MAT_CUST = "bidStatus.mergedTBiInfoMatCust";
+	
+	//BID_COMPLETE
+	public final static String QRY_SELECT_COMPLETE_EBID_LIST = "bidComp.selectCompleteEbidList";
+	public final static String QRY_SELECT_COMPLETE_EBID_DETAIL = "bidComp.selectCompleteEbidDetail";
+	public final static String QRY_SELECT_COMPLETE_EBID_JOIN_CUST_LIST = "bidComp.selectCompleteEbidJoinCustList";
+	public final static String QRY_SELECT_COMPLETE_EBID_JOIN_CUST_SPEC = "bidComp.selectCompleteEbidJoinCustSpec";
+	public final static String QRY_SELECT_COMPLETE_EBID_DETAIL_FILE = "bidComp.selectCompleteEbidDetailFile";
+	public final static String QRY_SELECT_COMPLETE_EBID_DETAIL_SPEC = "bidComp.selectCompleteEbidDetailSpec";
+	public final static String QRY_UPDATE_COMPLETE_EBID_REAL_AMT = "bidComp.updateCompleteEbidRealAmt";
+	public final static String QRY_SELECT_COMPLETE_EBID_LOTTE_MAT_CODE = "bidComp.selectCompleteEbidLotteMatCode";
+	public final static String QRY_SELECT_COMPLETE_EBID_HISTORY_LIST = "bidComp.selectCompleteEbidHistoryList";
+	public final static String QRY_SELECT_EBID_HISTORY_JOIN_CUST_LIST = "bidComp.selectEbidHistoryJoinCustList";
+	public final static String QRY_SELECT_PARTNER_COMPLETE_EBID_LIST = "bidComp.selectPartnerCompleteEbidList";
+	public final static String QRY_SELECT_PARTNER_COMPLETE_EBID_DETAIL = "bidComp.selectPartnerCompleteEbidDetail";
+	public final static String QRY_SELECT_PARTNER_COMPLETE_EBID_CUST_DETAIL = "bidComp.selectPartnerCompleteEbidCustDetail";
+	public final static String QRY_UPDATE_SUCC_EBID_CONFIRM = "bidComp.updateSuccEbidConfirm";
+	
 }
